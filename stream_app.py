b024fcb09dc7a18ce61d0c3bc5b52600f84b6b2d
Parag Bhattacharjee <psychoprg357@gmail.com>
2023-07-09 12:13:09 +0530

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -1,7 +1,6 @@
 import streamlit
 
-streamlit.title('parents healthy diner');
-streamlit.header('Menu');
-streamlit.text('oatmeal');
-streamlit.text('spinach');
-
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
