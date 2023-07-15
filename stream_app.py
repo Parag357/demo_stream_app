b48a67cab73a95ab91c7056ec36c53cbfc1695e5
Parag Bhattacharjee <psychoprg357@gmail.com>
2023-07-15 15:26:15 +0530

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -29,6 +29,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
+streamlit.stop() # stopping load to DB from UI
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("insert into FRUIT_LOAD_LIST values('from streamlit')")
